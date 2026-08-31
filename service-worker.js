5e0fd040b87385081679e50a13e91ec68c0fafb1
pauncatalin-cmd <pauncatalin@gmx.de>
2026-08-31 18:48:31 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+const CACHE="gymtrack-ro-v3";
+const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.svg","./icon-512.svg",
+"./assets/bankdruecken.jpg","./assets/brustpresse.jpg","./assets/butterfly.jpg","./assets/beinstrecker.jpg","./assets/beinbeuger.jpg"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))));
