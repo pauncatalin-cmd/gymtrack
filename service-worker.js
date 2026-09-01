2bdb5af2ebfbbbd0199483f7741e2f9f3d8e22dc
pauncatalin-cmd <pauncatalin@gmx.de>
2026-09-01 15:07:02 +0200

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE="gymtrack-final-v1";
-const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.svg","./icon-512.svg"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request))));
